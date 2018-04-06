1fa758b5ae28c55f429b75ac79f12c5b110352dd
svugerovajohanna <svuj00@vse.cz>
2018-04-06 23:19:29 +0200

    úvodní commit

---
+++ src/main/java/com/github/svugerovajohanna/vyuka_anglictiny/main/Start.java
@@ -0,0 +1,12 @@
+package com.github.svugerovajohanna.vyuka_anglictiny.main;
+
+/** Třída  Start bude třídou projektu,
+* který představuje jednoduchou aplikaci pro výuku angličtiny.
+*
+* @author    Johanna Švugerová
+* @version   LS 2017/2018
+*/
+
+public class Start {
+
+}
